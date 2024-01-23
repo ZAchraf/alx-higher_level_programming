d432f0df7f7ec3b1cbc5d2c4ed8abcc89437ab4e
Ztote Achraf <achraf.ztote@gmail.com>
2024-01-23 15:39:06 -0800

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,10 +5,10 @@
 class Square:
     """ defining it"""
     
-    def__init__(self, size):
+    def __init__(self, size):
         """Constructor.
 
         Args:
-            size: length of the side of the square.
+            size: length of the side.
         """
         self.__size = size

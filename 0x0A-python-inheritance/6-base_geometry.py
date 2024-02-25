c5eb6ce68c0bea28b7ced72cefeea70759f9e584
Ztote Achraf <achraf.ztote@gmail.com>
2024-02-25 06:02:33 -0800

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''BaseGeometry. Defining a basegeometry class'''
+
+
+class BaseGeometry:
+    '''Base Geometry.'''
+    def area(self):
+        '''not implemented'''
+        raise Exception('area() is not implemented')

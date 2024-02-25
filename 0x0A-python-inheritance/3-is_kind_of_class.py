ee1b7db24d760d1e62271f4dce8912470d001943
Ztote Achraf <achraf.ztote@gmail.com>
2024-02-25 05:47:17 -0800

    task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''Defining a class and a class checking Function'''
+
+
+def is_kind_of_class(obj, a_class):
+    '''Check if object is instance or an inhereted instance of a class '''
+    return isinstance(obj, a_class)

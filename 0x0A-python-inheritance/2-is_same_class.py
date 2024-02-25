5d14f8d0a5441766fa11d0fcffa600ca32a8e3db
Ztote Achraf <achraf.ztote@gmail.com>
2024-02-25 05:36:24 -0800

    task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''Let's define a class checking Function'''
+
+
+def is_same_class(obj, a_class):
+    '''It will check if an object is exactly an instance of a given class'''
+    return type(obj) == a_class

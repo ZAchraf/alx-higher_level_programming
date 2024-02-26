25661c4bc08328299d45067628ab099483c7bb78
Ztote Achraf <achraf.ztote@gmail.com>
2024-02-26 06:57:37 -0800

    task inpoup

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""student class."""
+
+
+class Student:
+    """student rep."""
+
+    def __init__(self, first_name, last_name, age):
+        """new Student init"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """dict rep of Student."""
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
